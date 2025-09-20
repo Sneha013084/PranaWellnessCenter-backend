40f560752172c417a9cc8239d60efa17c14570ee
Sneha <snehayur30@gmail.com>
2025-09-20 18:51:00 -0400

    updated

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,8 +17,8 @@ const protect = async (req, res, next) =>{
     try {
         token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user= await User.findById(decoded.id).select("-password");
-        next();
+   const userId = decoded.id || decoded.user?._id;
+      req.user = await User.findById(userId).select("-password");
     }catch(error){
         res.status(401).json({message : "Not authorized, token failed"});
     }
