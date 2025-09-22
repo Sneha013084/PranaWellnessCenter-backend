7df55ac3679a08cb771f47a01bb93a8e7dbb5aaf
Sneha <snehayur30@gmail.com>
2025-09-22 10:36:29 -0400

    corrcted

--- index.js
+++ index.js
@@ -34,9 +34,8 @@ const frontendPath = path.join(__dirname, "../frontend/dist");
 app.use(express.static(frontendPath));
 
 // Handle all other routes → send index.html for frontend routing
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen( PORT, () => console .log( `Server running on port ${PORT}`));
